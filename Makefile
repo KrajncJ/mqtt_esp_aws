0a0b45b20a88f520aabc9abbf21cbbf5cd8dd001
dp5306 <dp5306@student.uni-lj.si>
2020-05-15 10:18:00 +0200

    added graphs

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
 PROGRAM=aws_seminar
-EXTRA_COMPONENTS = extras/paho_mqtt_c extras/mbedtls extras/i2c extras/bmp280
+
+EXTRA_CFLAGS=-DLWIP_HTTPD_CGI=1 -DLWIP_HTTPD_SSI=1 -I./fsdata
+
+EXTRA_COMPONENTS = extras/paho_mqtt_c extras/mbedtls extras/i2c extras/bmp280 extras/httpd
+
 include ../../common.mk
+
+html:
+	@echo "Generating fsdata.."
+	cd fsdata && ./makefsdata
--- aws_seminar.c
+++ aws_seminar.c
@@ -1,5 +1,6 @@
 
 #include "espressif/esp_common.h"
+#include <esp8266.h>
 #include "esp/uart.h"
 #include <string.h>
 #include <FreeRTOS.h>
@@ -12,19 +13,20 @@
 #include "ssl_connection.h"
 #include "bmp280/bmp280.h"
 #include "i2c/i2c.h"
-
-
-
-
-
+#include <ssid_config.h>
+#include <httpd/httpd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // Put this values into wifi config file
-#define WIFI_NAME "***"
-#define WIFI_PASS "***"
+#define WIFI_NAME "SteinsGate;"
+#define WIFI_PASS "Danganronpa"
 
 // MQTT config // 
 #define mqtt_server "a2zttsshc1wnh8.iot.eu-west-1.amazonaws.com"
 #define MQTT_PORT 8883
+
+#define LED_PIN 2
 extern char *ca_cert, *client_cert, *client_key; // Certificates for amazon SSL connection
 
 // I2C config constants for I2C init (for sensor captuiring)
@@ -33,12 +35,15 @@ const uint8_t scl_pin = 14;
 const uint8_t sda_pin = 12;
 
 // Mesure after each interval
-const int MESURE_INTERVAL_MS = 15000; // Change this value if needed
+const int MESURE_INTERVAL_MS = 20000; // Change this value if needed
 static QueueHandle_t xQueue; // Queue for measures
+static bool MQTT_connection_established = false;
+static bool bmp280_init_successful = false;
 
 // Device MAC address for identification
 char device_mac[13];
 char mqtt_publish_topic[100];
+char mqtt_subscribe_topic[100];
 
 // SSL variables
 static int ssl_invalid;
@@ -50,6 +55,236 @@ struct Measure {
    float  air_pressure;
 };
 
+struct Device {
+   bool initialized;
+   char device_mac[13];
+   char last_measure[80];
+   float temperature;
+   float air_pressure;
+};
+
+static struct Device ConnectedDevices[2];
+
+enum {
+    SSI_UPTIME,
+    SSI_FREE_HEAP,
+    SSI_LED_STATE
+};
+
+int32_t ssi_handler(int32_t iIndex, char *pcInsert, int32_t iInsertLen)
+{
+    switch (iIndex) {
+        case SSI_UPTIME:
+            snprintf(pcInsert, iInsertLen, "%d",
+                    xTaskGetTickCount() * portTICK_PERIOD_MS / 1000);
+            break;
+        case SSI_FREE_HEAP:
+            snprintf(pcInsert, iInsertLen, "%d", (int) xPortGetFreeHeapSize());
+            break;
+        case SSI_LED_STATE:
+            snprintf(pcInsert, iInsertLen, (GPIO.OUT & BIT(LED_PIN)) ? "Off" : "On");
+            break;
+        default:
+            snprintf(pcInsert, iInsertLen, "N/A");
+            break;
+    }
+
+    /* Tell the server how many characters to insert */
+    return (strlen(pcInsert));
+}
+
+char *gpio_cgi_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[])
+{
+    for (int i = 0; i < iNumParams; i++) {
+        if (strcmp(pcParam[i], "on") == 0) {
+            uint8_t gpio_num = atoi(pcValue[i]);
+            gpio_enable(gpio_num, GPIO_OUTPUT);
+            gpio_write(gpio_num, true);
+        } else if (strcmp(pcParam[i], "off") == 0) {
+            uint8_t gpio_num = atoi(pcValue[i]);
+            gpio_enable(gpio_num, GPIO_OUTPUT);
+            gpio_write(gpio_num, false);
+        } else if (strcmp(pcParam[i], "toggle") == 0) {
+            uint8_t gpio_num = atoi(pcValue[i]);
+            gpio_enable(gpio_num, GPIO_OUTPUT);
+            gpio_toggle(gpio_num);
+        }
+    }
+    return "/index.ssi";
+}
+
+char *about_cgi_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[])
+{
+    return "/about.html";
+}
+
+char *websocket_cgi_handler(int iIndex, int iNumParams, char *pcParam[], char *pcValue[])
+{
+    return "/websockets.html";
+}
+
+void websocket_task_measurements(void *pvParameter)
+{
+    printf("----------------------------websocket_task_measurements-------------------------\n");
+    
+    struct tcp_pcb *pcb = (struct tcp_pcb *) pvParameter;
+
+    for (;;) {
+        if (pcb == NULL || pcb->state != ESTABLISHED) {
+            printf("-------------------------Connection closed, deleting task\n");
+            break;
+        }
+	printf("----------------------------websocket_task_measurements-------------------------\n");
+	for(int i = 0; i < 2 ; i++){
+		if(!ConnectedDevices[i].initialized)
+			break;
+		char response[112] = "{";
+        	int len = snprintf(response, sizeof (response),
+                "{\"mac\" : \"%s\","
+                " \"measurement\" : \"%s\"}", ConnectedDevices[i].device_mac, ConnectedDevices[i].last_measure);
+        	if (len < sizeof (response))
+            		websocket_write(pcb, (unsigned char *) response, len, WS_TEXT_MODE);
+		printf("%s\n", response);
+        	vTaskDelay(2000 / portTICK_PERIOD_MS);
+	}
+        /* Generate response in JSON format */
+        
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+    }
+
+    vTaskDelete(NULL);
+}
+
+void websocket_task(void *pvParameter)
+{
+    printf("websocket_task\n");
+    struct tcp_pcb *pcb = (struct tcp_pcb *) pvParameter;
+
+    for (;;) {
+        if (pcb == NULL || pcb->state != ESTABLISHED) {
+            printf("++++++++++++++++++++++++++Connection closed, deleting task\n");
+            break;
+        }
+
+	printf("++++++++++++++++++++++++++++++websocket_task_measurements+++++++++++++++++++++++++++++++\n");
+        int uptime = xTaskGetTickCount() * portTICK_PERIOD_MS / 1000;
+        int heap = (int) xPortGetFreeHeapSize();
+        int led = !gpio_read(LED_PIN);
+
+        /* Generate response in JSON format */
+        char response[64];
+        int len = snprintf(response, sizeof (response),
+                "{\"uptime\" : \"%d\","
+                " \"heap\" : \"%d\","
+                " \"led\" : \"%d\"}", uptime, heap, led);
+        if (len < sizeof (response))
+            websocket_write(pcb, (unsigned char *) response, len, WS_TEXT_MODE);
+
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+
+    vTaskDelete(NULL);
+}
+
+/**
+ * This function is called when websocket frame is received.
+ *
+ * Note: this function is executed on TCP thread and should return as soon
+ * as possible.
+ */
+void websocket_cb(struct tcp_pcb *pcb, uint8_t *data, u16_t data_len, uint8_t mode)
+{
+    printf("[websocket_callback]:\n%.*s\n", (int) data_len, (char*) data);
+
+    uint8_t response[18];
+    response[0] = (uint8_t) 9;
+
+    switch (data[0]) {
+        case '0':
+	    for(int i = 0; i < 2; i ++){
+		response[i*9 + 0] = (uint8_t) i;
+		
+		uint8_t* byte = (uint8_t*) &ConnectedDevices[i].temperature;
+
+		response[i*9 + 1] = byte[3];
+		response[i*9 + 2] = byte[2];
+		response[i*9 + 3] = byte[1];
+		response[i*9 + 4] = byte[0];
+
+		byte = (uint8_t*) &ConnectedDevices[i].air_pressure;
+
+		response[i*9 + 5] = byte[3];
+		response[i*9 + 6] = byte[2];
+		response[i*9 + 7] = byte[1];
+		response[i*9 + 8] = byte[0];
+	    }
+            break;
+        case 'D': // Disable LED
+            gpio_write(LED_PIN, true);
+            //val = 0xDEAD;
+            break;
+        case 'E': // Enable LED
+            gpio_write(LED_PIN, false);
+            //val = 0xBEEF;
+            break;
+        default:
+            printf("Unknown command\n");
+            //val = 0;
+            break;
+    }
+    websocket_write(pcb, response, 18, WS_BIN_MODE);
+//
+}
+
+/**
+ * This function is called when new websocket is open and
+ * creates a new websocket_task if requested URI equals '/stream'.
+ */
+void websocket_open_cb(struct tcp_pcb *pcb, const char *uri)
+{
+    printf("WS URI: %s\n", uri);
+    if (!strcmp(uri, "/stream")) {
+        printf("request for streaming\n");
+        //xTaskCreate(&websocket_task, "websocket_task", 256, (void *) pcb, 2, NULL);
+        //xTaskCreate(&websocket_task_measurements, "websocket_task_measurements", 256, (void *) pcb, 3, NULL);
+        //printf("request for streaming executed\n");
+    }
+}
+
+void httpd_task(void *pvParameters)
+{
+    while(!MQTT_connection_established){
+         printf("Delaying1\n");
+         vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    printf("httpd_task continue...\n");
+
+    tCGI pCGIs[] = {
+        {"/gpio", (tCGIHandler) gpio_cgi_handler},
+        {"/about", (tCGIHandler) about_cgi_handler},
+        {"/websockets", (tCGIHandler) websocket_cgi_handler},
+    };
+
+    const char *pcConfigSSITags[] = {
+        "uptime", // SSI_UPTIME
+        "heap",   // SSI_FREE_HEAP
+        "led"     // SSI_LED_STATE
+    };
+
+    /* register handlers and start the server */
+    http_set_cgi_handlers(pCGIs, sizeof (pCGIs) / sizeof (pCGIs[0]));
+    http_set_ssi_handler((tSSIHandler) ssi_handler, pcConfigSSITags, sizeof (pcConfigSSITags) / sizeof (pcConfigSSITags[0]));
+    websocket_register_callbacks((tWsOpenHandler) websocket_open_cb, (tWsHandler) websocket_cb);
+    httpd_init();
+
+    printf("http handlers set \n");
+    bmp280_init_successful = true;
+
+    for (;;)
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+}
 
 static int is_wifi_connected() {
 	if (sdk_wifi_station_get_connect_status() == STATION_GOT_IP ) {
@@ -99,9 +334,107 @@ static int mqtt_ssl_write(mqtt_network_t* n, unsigned char* buffer, int len,
     return r;
 }
 
+static void topic_received(mqtt_message_data_t *md) {
+
+    mqtt_message_t *message = md->message;
+    int i;
+
+    printf("Received: ");
+    for (i = 0; i < md->topic->lenstring.len; ++i)
+	printf("%c", md->topic->lenstring.data[i]);
+
+    printf(" = ");
+    for (i = 0; i < (int) message->payloadlen; ++i)
+	printf("%c", ((char *) (message->payload))[i]);
+    printf("\r\n");
+
+    char current_device_mac[12];
+    char temperature[6];
+    char air_pressure[9];
+
+    for(int j=0; j<12; j++)
+	current_device_mac[j] = md->topic->lenstring.data[15+j];
+    current_device_mac[11] = '\0';
+
+    for(int j=0; j<6; j++)
+	temperature[j] = ((char *) (message->payload))[18+j];
+    temperature[5] = '\0';
+    printf("temperature  %s \n", temperature);
+
+    for(int j=0; j<9; j++)
+	air_pressure[j] = ((char *) (message->payload))[43+j];
+    air_pressure[8] = '\0';
+
+    printf("air_pressure  %s \n", air_pressure);
+	
+
+    for(int l=0; l<2; l++){
+	// if device not yet initialized
+	if(!(ConnectedDevices[l]).initialized){
+
+		printf("// if device not yet initialized \n");
+
+		(ConnectedDevices[l]).initialized = true;
+		for(int j=0; j<12; j++)
+			(ConnectedDevices[l]).device_mac[j] = current_device_mac[j];
+
+		for (int k = 0; k < (int) message->payloadlen; ++k)
+			(ConnectedDevices[l]).last_measure[k] = ((char *)(message->payload))[k];
+
+		ConnectedDevices[l].temperature = atof(temperature);
+		ConnectedDevices[l].air_pressure = atof(air_pressure);
+
+		printf("temperature: %f \n air_pressure: %f \n", ConnectedDevices[l].temperature, ConnectedDevices[l].air_pressure);
+		
+		for(int j=0; j<12; j++)
+			printf("%c", (ConnectedDevices[l]).device_mac[j]);
+		printf("\n");
+
+		for (int k = 0; k < (int) message->payloadlen; ++k)
+			printf("%c", (ConnectedDevices[l]).last_measure[k]);
+		printf("\n");
+
+		break;
+	} 
+	// if device ALREADY initialized
+	else if (!strcmp((ConnectedDevices[l].device_mac + '\0'), (current_device_mac + '\0'))) {
+
+		printf("// // if device ALREADY initialized \n");
+		
+		for (int k = 0; k < (int) message->payloadlen; ++k)
+			(ConnectedDevices[l]).last_measure[k] = ((char *)(message->payload))[k];
+
+		ConnectedDevices[l].temperature = atof(temperature);
+		ConnectedDevices[l].air_pressure = atof(air_pressure);
+
+		printf("temperature: %s  air_pressure: %s", temperature, air_pressure);
+
+		for (int k = 0; k < (int) message->payloadlen; ++k)
+			printf("%c", (ConnectedDevices[l]).last_measure[k]);
+		printf("\n");
+
+	
+		break;
+
+	}
+	else {
+		printf("// if device NOT ALREADY initialized \n");
+
+		//for (int k = 0; k < 13; ++k)
+			printf("%s", current_device_mac);
+		printf("\n");
+
+		
+		//for (int k = 0; k < 13; ++k)
+			printf("%s", ConnectedDevices[l].device_mac);
+		printf("\n");
+	}
+    }
+}
+
 static void mqtt_task(void *pvParameters) {
     
-	int ret = 0;
+    int ret = 0;
 	
     struct mqtt_network network;
     uint8_t mqtt_buf[100];
@@ -111,7 +444,6 @@ static void mqtt_task(void *pvParameters) {
     mqtt_packet_connect_data_t data = mqtt_packet_connect_data_initializer;
     ssl_connection = (SSLConnection *) malloc(sizeof(SSLConnection));
     
-    
     while (1) {
     	printf("Is wifi alive (MQTT task) \n");
     	
@@ -120,7 +452,6 @@ static void mqtt_task(void *pvParameters) {
             vTaskDelay(1000 / portTICK_PERIOD_MS);
             continue;
         }
-
         printf("%s: started\n\r", __func__);
         ssl_invalid = 0;
         ssl_init(ssl_connection);
@@ -132,18 +463,19 @@ static void mqtt_task(void *pvParameters) {
         network.mqttread = mqtt_ssl_read;
         network.mqttwrite = mqtt_ssl_write;
 
-        printf("%s: connecting to MQTT server %s ... ", __func__,
-                mqtt_server);
+	//taskENTER_CRITICAL();
+        printf("%s: connecting to MQTT server %s ... ", __func__, mqtt_server);
         ret = ssl_connect(ssl_connection, mqtt_server, MQTT_PORT);
+	//taskEXIT_CRITICAL();
 
         if (ret) {
             printf("error: %d\n\r", ret);
             ssl_destroy(ssl_connection);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
             continue;
         }
-        printf("done\n\r");
-        mqtt_client_new(&client, &network, 5000, mqtt_buf, 100, mqtt_readbuf,
-                100);
+        printf("done connecting to MQTT server\n\r");
+        mqtt_client_new(&client, &network, 5000, mqtt_buf, 100, mqtt_readbuf, 100);
 
         data.willFlag = 0;
         data.MQTTVersion = 4;
@@ -157,12 +489,16 @@ static void mqtt_task(void *pvParameters) {
         if (ret) {
             printf("Error: %d \n", ret);
             ssl_destroy(ssl_connection);
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
             continue;
         }
         printf("Done \n");
   
+	mqtt_subscribe(&client, mqtt_subscribe_topic, MQTT_QOS1, topic_received);
 
+	MQTT_connection_established = true;
         while (wifi_ok && !ssl_invalid) {
+            printf("MQTT entering waiting for queue \n");
             char msg[64];
             struct Measure receivedMeasure;
             // Wait here for new measures...
@@ -194,6 +530,7 @@ static void mqtt_task(void *pvParameters) {
             }
 
             ret = mqtt_yield(&client, 1000);
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
             if (ret == MQTT_DISCONNECTED)
                 break;
         }
@@ -205,8 +542,11 @@ static void mqtt_task(void *pvParameters) {
 
 static void bmp280_task_forced(void *pvParameters)
 {	
-	
-	printf("bmp280_task_forced start... \n");
+    while(!MQTT_connection_established || !bmp280_init_successful){
+            printf("Delaying2\n");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("bmp280_task_forced continue... \n");
 	
     float pressure, temperature;
     
@@ -227,25 +567,26 @@ static void bmp280_task_forced(void *pvParameters)
             vTaskDelay(1000 / portTICK_PERIOD_MS);
         }
         
+	//bmp280_init_successful = true;
+
         // Measure loop
         while(1) {
-        	
         	printf("Messuring... \n");
-        	vTaskDelay(3000 / portTICK_PERIOD_MS);
         	
 
-        	// Force measure
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            // Force measure
             if (!bmp280_force_measurement(&bmp280_dev)) {
                 printf("Force measure init failed \n");
+	    	vTaskDelay(MESURE_INTERVAL_MS / portTICK_PERIOD_MS);
                 break;
             }
             
             // Wait for measurement
-            while (bmp280_is_measuring(&bmp280_dev)) {};
+            while (bmp280_is_measuring(&bmp280_dev)) { vTaskDelay(500 / portTICK_PERIOD_MS); };
 
             if (!bmp280_read_float(&bmp280_dev, &temperature, &pressure, NULL)) {
                 printf("Reading temp failed \n");
+	    	vTaskDelay(MESURE_INTERVAL_MS / portTICK_PERIOD_MS);
                 break;
             }
             
@@ -260,8 +601,7 @@ static void bmp280_task_forced(void *pvParameters)
             
      
             // Delay measures
-			vTaskDelay(MESURE_INTERVAL_MS / portTICK_PERIOD_MS);
-   
+	    vTaskDelay(MESURE_INTERVAL_MS / portTICK_PERIOD_MS);
         }
     }
 }
@@ -270,7 +610,7 @@ static void bmp280_task_forced(void *pvParameters)
 
 void initialize_wifi_connection() {
 
-    printf("Establishing WI-FI connection ...");
+    printf("Establishing WI-FI connection ... ");
 
     struct sdk_station_config config = { .ssid = WIFI_NAME, .password =
     		WIFI_PASS, };
@@ -293,8 +633,8 @@ void user_init(void) {
     snprintf(device_mac, sizeof(device_mac)-1, "%s", get_station_mac());
     
     // Init publish topic
-    snprintf(mqtt_publish_topic, sizeof(mqtt_publish_topic), "sensor/outside/%s/measures",
-                		device_mac);
+    snprintf(mqtt_publish_topic, sizeof(mqtt_publish_topic), "sensor/outside/%s/measures", device_mac);
+    snprintf(mqtt_subscribe_topic, sizeof(mqtt_subscribe_topic), "sensor/outside/+/measures");
                 
     printf("Device MAC: ");
     printf(device_mac);
@@ -302,6 +642,10 @@ void user_init(void) {
     printf("Publish topic: ");
     printf(mqtt_publish_topic);
     printf("\n");
+    printf("Subscribe topic: ");
+    printf(mqtt_subscribe_topic);
+    printf("\n");
+    
     
     // INIT I2C for sensors
     i2c_init(i2c_bus, scl_pin, sda_pin, I2C_FREQ_400K);
@@ -309,8 +653,13 @@ void user_init(void) {
     // Create queue to hold sensor data
     xQueue = xQueueCreate(3, sizeof(struct Measure));
 
+    /* turn off LED */
+    gpio_enable(LED_PIN, GPIO_OUTPUT);
+    gpio_write(LED_PIN, true);
     
+    /* initialize tasks */
     xTaskCreate(&bmp280_task_forced, "bmp280_task_forced", 1048, NULL, 2, NULL);
     xTaskCreate(&mqtt_task, "mqtt_task", 2048, NULL, 2, NULL);
+    xTaskCreate(&httpd_task, "httpd_task", 128, NULL, 3, NULL);
 
 }
